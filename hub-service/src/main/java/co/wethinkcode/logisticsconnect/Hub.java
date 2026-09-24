ca1483bd1e64937d9341b57b88cd933aae3b39bf
Zinhle Hlongwane <zinhlehlongwani56@gmail.com>
2026-09-24 05:26:46 +0200

    Add hub model for service integration

---
+++ hub-service/src/main/java/co/wethinkcode/logisticsconnect/Hub.java
@@ -0,0 +1,28 @@
+package co.wethinkcode.logisticsconnect;
+
+public class Hub {
+
+    private String id;
+    private String province;
+    private String sortingCenter;
+    private Boolean active;
+
+    public Hub() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getSortingCenter() {
+        return sortingCenter;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+}
